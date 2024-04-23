36a59ed0c3d55c33921fb379fdcc466331939be9
juanDcuatindioyN <126802980+juanDcuatindioyN@users.noreply.github.com>
2024-04-22 20:54:09 -0500

    completo

--- Gestor_Tutoriales/src/main/java/com/mycompany/gestor_tutoriales/GestionarTutoriales.java
+++ Gestor_Tutoriales/src/main/java/com/mycompany/gestor_tutoriales/GestionarTutoriales.java
@@ -25,7 +25,7 @@ public class GestionarTutoriales {
 
         String url = "jdbc:mysql://localhost:3306/?serverTimeZone=utc";
         String user = "root";
-        String password = "Neider2003";
+        String password = "1085250701";
         Connection conn = null;
 
         try {
